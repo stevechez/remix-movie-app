85277a4033ea4936c90028f098aba7adec53b61f
Steve M <stevechez@gmail.com>
2022-07-26 11:26:34 -0700

    initial commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,8 @@
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1 className="text-6xl font-black underline">Welcome to Remix</h1>
+      
+    </div>
+  );
+}
