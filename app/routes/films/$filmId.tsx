6274d6797c2188d53daf0000c5b6125c4aed1414
Steve M <stevechez@gmail.com>
2022-07-27 10:35:49 -0700

    updates to app

--- app/routes/films/$filmId.tsx
+++ app/routes/films/$filmId.tsx
@@ -1,16 +1,82 @@
-import type { LoaderFunction } from "@remix-run/node";
-import type * as films from "~/api/films";
+import type {
+  ActionFunction,
+  LoaderFunction,
+  MetaFunction,
+} from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+import { Outlet, useLoaderData } from "@remix-run/react";
+import type { Film } from "~/api/films";
 import { getFilmById } from "~/api/films";
 import invariant from "tiny-invariant";
-import { useLoaderData } from "@remix-run/react";
+import FilmBanner from "~/components/FilmBanner";
+import CharacterList from "~/components/CharacterList";
+// import CommentsList from "~/components/CommentsList";
+import { addComment } from "~/api/comments";
+
+export const action: ActionFunction = async ({ request, params }) => {
+  invariant(params.filmId, "expected params.filmId");
+  const body = await request.formData();
+
+  const comment = {
+    name: body.get("name") as string,
+    message: body.get("message") as string,
+    filmId: params.filmId,
+  };
+
+  const errors = { name: "", message: "" };
+
+  if (!comment.name) {
+    errors.name = "Please provide your name";
+  }
+
+  if (!comment.message) {
+    errors.message = "Please provide a comment";
+  }
+
+  if (errors.name || errors.message) {
+    const values = Object.fromEntries(body);
+    return { errors, values };
+  }
+
+  await addComment(comment);
+
+  return redirect(`/films/${params.filmId}`);
+};
+
+export const meta: MetaFunction = ({ data }) => {
+  return { title: data.title, description: data.description };
+};
 
 export const loader: LoaderFunction = async ({ params }) => {
   invariant(params.filmId, "expected params.filmId");
+
   const film = await getFilmById(params.filmId);
+
+  console.log("fetching film... -->", film.title);
+
   return film;
 };
 
+// eslint-disable-next-line @typescript-eslint/no-redeclare
 export default function Film() {
-  const film = useLoaderData<films.Film>();
-  return <div>{film.title}</div>;
+  const film = useLoaderData<Film>();
+  return (
+    <div>
+      <FilmBanner film={film} />
+
+      <div className="p-10">
+        <p>{film.description}</p>
+
+        <div className="flex py-5 space-x-5">
+          <CharacterList characters={film.characters} />
+
+          <div className="flex-1 flex flex-col justify-between">
+            <Outlet />
+
+            {/* <CommentsList filmId={film.id} comments={film.comments || []} /> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ app/routes/films/$filmid/characters.$characterid.tsx
@@ -0,0 +1,67 @@
+import { useCatch, useLoaderData } from "@remix-run/react";
+import type { json, LoaderFunction } from "@remix-run/node";
+import invariant from "tiny-invariant";
+import type { FilmCharacter } from "~/api/films";
+import { getFilmCharacter } from "~/api/films";
+
+export let loader: LoaderFunction = async ({ params }) => {
+  invariant(params.characterId, "expected params.characterId");
+
+  return getFilmCharacter(params.characterId);
+};
+
+export default function Character() {
+  const characterDetails = useLoaderData<FilmCharacter>();
+  return (
+    <div className="mb-3">
+      <div className="text-3xl mb-2">Character Details</div>
+      <div className="p-4 rounded shadow-lg border">
+        <div className="text-gray-700 font-bold text-xl mb-2">
+          {characterDetails.name}
+        </div>
+        <ul className="py-2">
+          <li>Gender: {characterDetails.gender}</li>
+          <li>Age: {characterDetails.age}</li>
+          <li>Eye Color: {characterDetails.eye_color}</li>
+          <li>Hair Color: {characterDetails.hair_color}</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+
+  if (caught.status === 404) {
+    return (
+      <div className="mb-3">
+        <div className="text-3xl mb-2">Details</div>
+        <div className="p-4 rounded shadow-lg border bg-orange-200 border-orange-600">
+          <div className="text-gray-700 font-bold text-xl mb-2">
+            {caught.statusText}
+          </div>
+          <p>
+            {caught.status} {caught.statusText}
+          </p>
+        </div>
+      </div>
+    );
+  }
+
+  throw new Error("Unkown error");
+}
+
+export function ErrorBoundary({ error }: any) {
+  return (
+    <div className="mb-3">
+      <div className="text-3xl mb-2">Details</div>
+      <div className="p-4 rounded shadow-lg border bg-rose-200 border-rose-600">
+        <div className="text-gray-700 font-bold text-xl mb-2">
+          Uh oh... Sorry something went wrong!
+        </div>
+        <p>{error?.message}</p>
+      </div>
+    </div>
+  );
+}
